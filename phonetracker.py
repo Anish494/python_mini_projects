af1a201a685bacb78d8dd18ed525ee7b19edeb35
Anish <anishks494@gmail.com>
2024-12-03 05:44:23 +0545

    some mini python projects

---
+++ phonetracker.py
@@ -0,0 +1,17 @@
+# import phonenumbers
+# from phonenumbers import geocoder
+# from phonetrackernumber import number
+# import folium
+
+# check_number=phonenumbers.parse(number)
+# number_location=geocoder.description_for_number(check_number,"en")
+# print(number_location)
+
+
+# from phonenumbers import carrier
+# service_provider=phonenumbers.parse(number)
+# print(carrier.name_for_number(service_provider,"en"))
+
+
+
+# from opencage.geocoder import OpenCageGeocode
---
+++ sendwhatsappmsgauto.py
@@ -0,0 +1,2 @@
+import pywhatkit as pyw
+pyw.sendwhatmsg("+9779815858334","This message was sent automatically using python.",21,26)
